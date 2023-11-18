cb929ce57a95f1d74266d9a9dada7977b5ea631b
Edson Suraty <edinhosuraty@gmail.com>
2023-11-18 15:39:33 -0300

    Cria getters e classe CLT

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -28,25 +28,53 @@ class Funcionarios {
     private _cpf: string;
     private _formacao: string;
     private _cargaHoraria: number;
-    private _remuneracao: number;
-
-    constructor(_nome:string,_idade:number, _cpf:string, _cargaHoraria: number, _remuneracao: number){
+    
+    constructor(_nome:string,_idade:number, _cpf:string, _cargaHoraria: number){
         this._nome = _nome;
         this._idade = _idade;
         this._cpf = _cpf;
-        this._formacao = "concluido";
+        this._formacao = "concluído";
         this._cargaHoraria = _cargaHoraria;
-        this._remuneracao = _remuneracao
+    }
+
+    get nome(){
+        return this._nome;
+    } 
+    get idade(){
+        return this._idade;
+    } 
+    get cpf(){
+        return this._cpf;
+    } 
+    get formacao(){
+        return this._formacao;
+    } 
+    get cargaHoraria(){
+        return this._cargaHoraria
     }
 
     gerarRemuneracao(){
-        const desconto = this._remuneracao * 0.1
-        const salarioFinal = this._remuneracao - desconto
-        return salarioFinal
-    }
 
+    }
 }
 
-const funcionario1 = new Funcionarios ('Edson', 24, '14982405778', 40, 2000)
+class CLT extends Funcionarios{
+    private _cargo: string
+    private _salario: number
+    constructor(_nome:string,_idade:number, _cpf:string, _cargaHoraria: number,  _cargo:string, _salario:number){
+        super(_nome,_idade, _cpf, _cargaHoraria)
+        this._cargo = _cargo;
+        this._salario = _salario
+    }
 
-console.log(funcionario1)
+    gerarRemuneracao(): string{
+        const desconto = this._salario * 0.1
+        const salarioFinal = this._salario - desconto
+        return `\nSeu salário é: R$${salarioFinal}` 
+    }
+}
+
+const clt1 = new CLT ('Edson', 24, "149.824.057-78", 40, "Dev Back-End Junior", 3000)
+
+console.log(clt1)
+console.log(clt1.gerarRemuneracao())
