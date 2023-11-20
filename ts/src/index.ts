ee5d94468de88d4c7ed42070e2c76ee2c083d3a7
Edson Suraty <edinhosuraty@gmail.com>
2023-11-20 20:42:12 -0300

    Tira poluição do código

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,27 +1,3 @@
-// Crie as entidades(classes) necessárias para um sistema que faça o controle de funcionários de uma empresa. 
-// Essa empresa conta com dois tipos de contrato, CLT e estágio, e deve representar os atributos de seus funcionários
-// de acordo com seu tipo de contrato, como carga horária, cargo, formação, dados pessoais.
-
-// Esse sistema, deve ser capaz de gerar o contra cheque de cada funcionário, deve também adicionar, remover e listar
-// os funcionários
-
-// Crie as entidades necessárias para um sistema que faça o controle de funcionários de uma empresa:
-
-//     * Um funcionário deve ter dados pessoais(nome, idade, etc.)
-//     * Um funcionário devem ter uma carga horária
-//     * Um CLT devem ter uma formação
-//     * Um CLT devem ter um cargo
-//     * Um CLT deve ter um atributo salário
-//     * Um estagiário deve ter um atributo "bolsa"(salário)
-//     * Um estagiário deve ter um atributo formação
-//     * Um estagiário deve ter um uma área de atuação
-//     * Um estagiário deve ter um atributo status da formação ("cursando")
-//     * A classe funcionário deve conter um método "gerarRemuneracao"
-//     * Funcionários CLT tem desconto no pagamento, e deve ser calculado na hora de gerar a remuneração
-
-//     * É preciso ter uma listagem de funcionários, para isso utilize uma classe Funcionários 
-//         * A classe Funcionarios deve métodos para adicionar e remover um funcionário
-
 interface DadosPessoais {
     nome: string;
     idade: number;
@@ -150,7 +126,7 @@ class Estagiario extends Funcionarios{
 const clt1 = new CLT ({
     nome: 'Edson', 
     idade: 24, 
-    cpf: "149.824.057-78", 
+    cpf: "000.000.000-00", 
     formacao: "Ciência da Computação", 
     cargaHoraria: 40, 
     cargo: "Dev Back-End Junior", 
@@ -167,10 +143,11 @@ const estagiario1 = new Estagiario ({
 
 Funcionarios.adicionarFuncionario(clt1)
 console.log(Funcionarios.listarFuncionarios())
-console.log(clt1.gerarRemuneracao())
+console.log(`Salário CLT abaixo: ${clt1.gerarRemuneracao()}`)
 
 Funcionarios.adicionarFuncionario(estagiario1)
 console.log(Funcionarios.listarFuncionarios())
+console.log(`Salário Estagiário abaixo: ${estagiario1.gerarRemuneracao()}`)
 Funcionarios.removerFuncionario(estagiario1)
 
 console.log(Funcionarios.listarFuncionarios())
